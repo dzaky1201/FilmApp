cff3f961289801f758a44cd680257dc49776f03e
Dzaky Haidar <dzakyhdr@gmail.com>
2021-02-28 12:45:22 +0700

    testingRoom

--- app/src/main/java/com/dzakyhdr/filmapp/data/model/movie/Movie.kt
+++ app/src/main/java/com/dzakyhdr/filmapp/data/model/movie/Movie.kt
@@ -13,8 +13,6 @@ data class Movie(
     val id: Int,
     @SerializedName("overview")
     val overview: String,
-    @SerializedName("popularity")
-    val popularity: Double,
     @SerializedName("poster_path")
     val posterPath: String,
     @SerializedName("release_date")
